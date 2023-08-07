0c31361bf5c2b42f0b09a0a669eb7d01e064d412
Krishnaingle7 <krishnaingle58@gmail.com>
2023-08-07 09:47:23 +0530

    first commit

---
+++ Pattern6.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class Pattern6 {
+
+	public static void main(String[] args) {
+		// For X in box pattern
+		int n=11;
+		for( int i=0; i<n ; i++)
+		{
+			for( int j=0; j<n; j++)
+			{
+				if(i==j || i+j==n-1 || i==0 || j==0 || i==n-1 || j==n-1)
+				{
+					System.out.print("*");
+				}
+				else
+				{
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+
+	}
+
+}
